9c8a5e34b268b0ac2d6d77aea212542342d86ac3
Arthur Cruz <arthur.cruz@esig.com.br>
2022-03-25 15:42:42 -0300

    integration tests completed

---
+++ src/test/java/br/com/artcruz/codeminerchallenge/integration/controller/ResourceControllerTest.java
@@ -0,0 +1,40 @@
+package br.com.artcruz.codeminerchallenge.integration.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ResourceControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void list() throws Exception {
+		mockMvc.perform(get("/resources")).andDo(print()).andExpect(status().isOk());
+	}
+
+	@Test
+	public void find() throws Exception {
+		mockMvc.perform(get("/resources/1")).andDo(print()).andExpect(status().isOk());
+	}
+
+	@Test
+	public void add() throws Exception {
+		String jsonString = "{ \"name\": \"MINERALS\", \"weight\": 10, \"contract_id\": 1 }";
+
+		mockMvc.perform(post("/resources").contentType(MediaType.APPLICATION_JSON).content(jsonString)).andDo(print())
+				.andExpect(status().isCreated());
+	}
+}
